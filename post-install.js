636aa7bc3c731788a36c5482cc7337c992deff5d
Ricky Clegg <me@rickyclegg.com>
2014-05-29 15:36:50 +0100

    Changing how I call bower

--- post-install.js
+++ post-install.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV,
     spawn = require('child_process').spawn;
 
 if (env === 'production') {
-    var spawnProcess = spawn('bower install');
+    var spawnProcess = spawn('./node_modules/bower/bin/bower install');
 
     spawnProcess.stderr.on('data', function(data) {
         console.log('something went wrong installing deps for ' + path + '.  Error: ', data);
