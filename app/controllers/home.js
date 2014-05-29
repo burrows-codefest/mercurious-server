6e27f145bf82be61b01817e6e40339f8f1ba363a
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-05-29 12:04:06 +0100

    Remove console log and change view to html.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose'),
 
 exports.index = function(req, res){
   Article.find(function(err, articles){
-      console.log(article);
     if(err) throw new Error(err);
     res.render('home/index', {
       title: 'Generator-Express MVC',
