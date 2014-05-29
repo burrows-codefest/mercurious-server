d6fb8f8a12b4877aeb71917e8f9d88407b413ad3
Ricky Clegg <me@rickyclegg.com>
2014-05-29 17:23:10 +0100

    Basic traffic warning

--- config/routes.js
+++ config/routes.js
@@ -4,12 +4,19 @@ module.exports = function(app, io){
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
 
+    function trafficWarningEngine(sockets) {
+        setInterval(function () {
+            sockets.emit('trafficWarning', 'Traffic Problem');
+        }, 10000);
+    }
 
     //socket IO
     io.sockets.on('connection', function (socket) {
         socket.on('message', function (data) {
             io.sockets.emit('message', data);
         });
+
+        trafficWarningEngine(io.sockets);
     });
 
 };
---
+++ public/scripts/directives/trafficWarning.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('mercuriousApp')
+    .directive('merTrafficWarning', function ($timeout, socket) {
+        return {
+            restrict: 'E',
+            templateUrl: '/scripts/directives/templates/traffic-warning.html',
+            link: function (scope) {
+                socket.on('trafficWarning', function () {
+                    scope.warning = 'Traffic Update';
+
+                    scope.showWarning = true;
+
+                    $timeout(function () {
+                        scope.showWarning = false;
+                    }, 3000);
+                });
+            }
+        };
+    });
