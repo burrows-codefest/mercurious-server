a8f0f7be66a3c87d76bdb689779002fe9aafc550
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-05-29 17:20:25 +0100

    added a github webhook

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,14 @@
 module.exports = function(app, io){
 
+    //Github Webhook
+    app.get('/api/github', function (req, res) {
+        io.sockets.on('message', req.body);
+        res.send('200');
+    });
+
 	//home route
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
-
 
     //socket IO
     io.sockets.on('connection', function (socket) {
