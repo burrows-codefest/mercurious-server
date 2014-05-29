d550f81de24be14ef9d90b8cc9cfc71883e57ab0
yordaniliev <yordan.iliev@burrows.yr.com>
2014-05-29 17:48:48 +0100

    Meme generator

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -18,4 +18,39 @@ angular.module('mercuriousApp')
                 $scope.feed = res.responseData.feed.entries;
             });
         });
+    })
+    .controller('MemeCtrl', function ($scope, $http, socket) {
+
+        $scope.memeLine1 = "The longer you stare";
+        $scope.memeLine2 = "the funnier it gets";
+
+        $scope.$watch('memeImgUrl', function(newVal){
+
+            if (newVal == undefined || newVal == "") {
+                newVal = "/images/meme.jpg";
+            }
+
+            $scope.memeImgWrapperStyles = {"background-image": "url('" + newVal + "')"};
+
+        });
+
+        $scope.submit = function() {
+
+            var memeObj = {};
+
+            memeObj.line1 = $scope.memeLine1;
+            memeObj.line2 = $scope.memeLine2;
+
+            if ($scope.memeImgUrl == undefined || $scope.memeImgUrl == "") {
+                memeObj.imgUrl = "/images/meme.jpg";
+            }
+            else {
+                memeObj.imgUrl = $scope.memeImgUrl;
+            }
+
+
+            socket.emit('meme', memeObj);
+
+        };
+
     });
