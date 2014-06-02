5d99c7c3823f8b9661761eb26fa53c9ef7c24222
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-06-02 09:59:50 +0100

    Remove old feed and add IO feed to template.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,23 +1,9 @@
 'use strict';
 
 angular.module('mercuriousApp')
-    .controller('MainCtrl', function ($scope, socket, dataSourceService, rssFeedService) {
-
-        $scope.newValue = 'edit socket';
-
+    .controller('MainCtrl', function ($scope, socket) {
         socket.on('message', function (data) {
-            console.log(data);
-            $scope.test = data;
-        });
-
-        $scope.editSocket = function() {
-            socket.emit('message', $scope.newValue);
-        };
-
-        dataSourceService.getSources().forEach(function(feed) {
-            rssFeedService.getFeed(feed.url).then(function (res) {
-                $scope.feed = res.responseData.feed.entries;
-            });
+            $scope.feed = data;
         });
     })
     .controller('MemeCtrl', function ($scope, $http, socket) {
