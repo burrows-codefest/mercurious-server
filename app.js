6f3f5b0feee0650b0d4f6709cdec2bfbe3ef39f7
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-06-24 15:59:01 +0100

    removed multiple vars from app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express = require('express'),
+var db,
+    express = require('express'),
     mongoose = require('mongoose'),
     fs = require('fs'),
     config = require('./config/config'),
-    socketIO = require('./app/controllers/socketIO');
+    socketIO = require('./app/controllers/socketIO'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    servicesPath = __dirname + '/app/services';
 
 mongoose.connect(config.db);
-var db = mongoose.connection;
+db = mongoose.connection;
 db.on('error', function () {
     throw new Error('unable to connect to database at ' + config.db);
 });
-
-var app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
 
 require('./config/express')(app, config);
 require('./config/routes')(app, io);
 
 socketIO.startPingToClients(io);
 
-var servicesPath = __dirname + '/app/services';
 fs.readdirSync(servicesPath).forEach(function (file) {
     var service;
     if (file.indexOf('.js') >= 0) {
