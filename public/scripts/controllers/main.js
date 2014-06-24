a8275a57dae6ad675d681f2ac257d3e96f351530
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-06-24 08:10:16 +0100

    Remove buggy Forza Feed

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -18,10 +18,8 @@ function getEasterEgg(race) {
 }
 
 angular.module('mercuriousApp')
-    .controller('MainCtrl', function ($scope, socket, $firebase, raceReportService, dbService) {
-        var raceReportsLoaded = false,
-            raceReportsRef = new Firebase('https://forza.firebaseio.com/raceReports/');
-        
+    .controller('MainCtrl', function ($scope, socket, dbService) {
+
         $scope.createMeme = false;
         $scope.feed = [];
 
@@ -48,31 +46,6 @@ angular.module('mercuriousApp')
         });
         socket.on('ping', function () {
             socket.emit('pong');
-        });
-        
-        $scope.raceReports = $firebase(raceReportsRef);
-        
-        $scope.raceReports.$on('loaded', function(races) {
-            for (var race in races) {
-                if (races[race]) {
-                    $scope.feed.push(raceReportService.compileRaceReport(races[race]));
-
-                    if (isRickyLast(races[race].result)) {
-                        $scope.feed.push(getEasterEgg(races[race]));
-                    }
-                }
-            }
-            raceReportsLoaded = true;
-        });
-        
-        $scope.raceReports.$on('change', function(raceId) {
-            if (raceReportsLoaded === true && $scope.raceReports[raceId]) {
-                $scope.feed.push(raceReportService.compileRaceReport($scope.raceReports[raceId]));
-                
-                if (isRickyLast($scope.raceReports[raceId].result)) {
-                    $scope.feed.push(getEasterEgg($scope.raceReports[raceId]));
-                }
-            }
         });
 
         $scope.sendVote = function(memeId, action) {
