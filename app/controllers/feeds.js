8919914bbacbef894199a106a92a0000555aaa01
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-06-27 11:43:38 +0100

    added tests for feeds controller

--- app/controllers/feeds.js
+++ app/controllers/feeds.js
@@ -6,19 +6,23 @@ exports.loadAllFeeds = function (socket) {
     FeedModel.find()
         .sort('type')
         .exec(function (err, results) {
-            var type = '', itemCount, items = {};
+            var type = '',
+                itemCount,
+                items = [];
+
             results.forEach(function (item) {
                 if (item.type !== type) {
                     itemCount = 0;
                     type = item.type;
-                    items[item.type] = [];
                 }
+
                 if (itemCount < 20 && type === item.type) {
-                    items[type].push(item);
+                    items.push(item);
                     itemCount += 1;
                 }
             });
-            socket.emit(constants.SOCKET.MESSAGE, results);
+
+            socket.emit(constants.SOCKET.MESSAGE, items);
         });
 
 };
