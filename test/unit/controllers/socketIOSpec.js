2b16708c9801f8448a57d485669407b85e9fce40
Jason Jeffrey <jason.jeffrey@burrows.yr.com>
2014-06-30 11:26:34 +0100

    written tests for socketIO controller

--- test/unit/controllers/socketIOSpec.js
+++ test/unit/controllers/socketIOSpec.js
@@ -1,8 +1,7 @@
 'use strict';
 describe('SocketIO Controller', function () {
     var socketIO, constants, rewire,
-        socketIOPath = '../../../app/controllers/socketIO',
-        feedHandlerPath = '../../../app/controllers/messageHandlers/feedHandler';
+        socketIOPath = '../../../app/controllers/socketIO';
 
     before(function () {
         rewire = require('rewire');
@@ -15,7 +14,6 @@ describe('SocketIO Controller', function () {
 
     afterEach(function () {
         delete require.cache[require.resolve(socketIOPath)];
-        delete require.cache[require.resolve(feedHandlerPath)];
     });
 
     it('should not be undefined', function () {
@@ -23,20 +21,85 @@ describe('SocketIO Controller', function () {
     });
 
     describe('incomingMessage Function', function () {
-        it('should send client to home page', function () {
-            var feedHandler = rewire(feedHandlerPath),
-                feedHandlerMock = sinon.spy(),
+        it('should find the feed handler and send data to the handleMessage function ', function () {
+            var feedHandlerPath = '../../../app/controllers/messageHandlers/feedHandler',
+                feedHandler = require(feedHandlerPath),
                 socksMock = 'this is a socks mock',
                 socketEmitSpy = sinon.spy();
 
-            feedHandler.__set__(feedHandlerMock);
+            feedHandler.handleMessage = sinon.spy();
 
             socketIO.incomingMessage(socksMock, {emit: socketEmitSpy}, {type: 'feed'});
 
-            expect(renderSpy.called).to.be.ok;
-            expect(renderSpy.args[0][0]).to.equal(constants.TEMPLATE.INDEX);
+            expect(feedHandler.handleMessage.called).to.be.ok;
+            expect(feedHandler.handleMessage.args[0][0]).to.equal(socksMock);
+
+            delete require.cache[require.resolve(feedHandlerPath)];
+        });
+
+        it('has no message type passed', function () {
+            var socketEmitSpy = sinon.spy();
+
+            socketIO.incomingMessage('', {emit: socketEmitSpy}, {});
+
+            expect(socketEmitSpy.called).to.be.ok;
+            expect(socketEmitSpy.args[0][0]).to.equal(constants.SOCKET.ERROR);
         });
 
     });
 
+    describe('outgoingMessage Function', function () {
+
+        function getSocksMock () {
+            return {
+                to: sinon.spy(function () { return this;}),
+                emit: sinon.spy()
+            }
+        }
+
+        it('should emit a global message to all connected users ', function () {
+            var socksMock = getSocksMock();
+
+            socketIO.outgoingMessage(socksMock, {type: 'feed'});
+
+            expect(socksMock.to.called).to.be.ok;
+            expect(socksMock.emit.called).to.be.ok;
+            expect(socksMock.emit.args[0][0]).to.equal(constants.SOCKET.NEW_ITEM);
+        });
+    });
+
+    describe('startPingToClients Function', function () {
+
+        var clock;
+
+        beforeEach(function () {
+            clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function () {
+            clock.restore();
+        });
+
+        function getSocksMock () {
+            return {
+                to: sinon.spy(function () { return this;}),
+                emit: sinon.spy()
+            }
+        }
+
+        it('should ping the client every 10 seconds ', function () {
+            var socksMock = getSocksMock();
+
+            socketIO.startPingToClients(socksMock);
+            expect(socksMock.emit.called).to.not.be.ok;
+
+            socketIO.startPingToClients = sinon.spy();
+
+            clock.tick(10010);
+
+            expect(socksMock.to.called).to.be.ok;
+            expect(socksMock.emit.called).to.be.ok;
+            expect(socketIO.startPingToClients.called).to.be.ok;
+        });
+    });
 });
