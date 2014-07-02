c152e0bc68ce762055926c7bde2b1bccab5fd451
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-07-02 14:39:06 +0100

    Create CR overview directive

---
+++ public/scripts/directives/codeReviewStatus.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('mercuriousApp')
+    .directive('merCodeReviewStatus', function (socket) {
+        return {
+            restrict: 'E',
+            templateUrl: '/scripts/directives/templates/code-review-status.html',
+            link: function (scope) {
+                scope.pullRequests = [];
+
+                socket.emit('githubAllPullRequest', {type: 'github'});
+
+                socket.on('githubAllPullRequest', function (data) {
+                    scope.pullRequests = data;
+                    console.log(data);
+                });
+
+                socket.on('githubNewPullRequest', function(data) {
+                    scope.pullRequests.push(data);
+                });
+
+                socket.on('githubUpdatedPullRequest', function(data) {
+                    scope.pullRequests.push(data);
+                });
+            }
+        };
+    });
