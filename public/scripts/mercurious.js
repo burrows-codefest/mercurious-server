f29c617378cef06bc279043ed74460ec32f7e58d
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-07-08 16:30:47 +0100

    more meme restructure

--- public/scripts/mercurious.js
+++ public/scripts/mercurious.js
@@ -13,20 +13,24 @@ angular
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl'
             })
+            .when('/memes/', {
+                templateUrl: 'views/memes/main.html',
+                controller: 'MainCtrl'
+            })
             .when('/memes/create-meme/', {
-                templateUrl: 'views/create-meme.html',
+                templateUrl: 'views/memes/create-meme.html',
                 controller: 'CreateMemeCtrl'
             })
             .when('/memes/create-meme/:context', {
-                templateUrl: 'views/create-meme.html',
+                templateUrl: 'views/memes/create-meme.html',
                 controller: 'CreateMemeCtrl'
             })
             .when('/memes/meme/:id', {
-                templateUrl: 'views/view-meme.html',
+                templateUrl: 'views/memes/view-meme.html',
                 controller: 'ViewMemeCtrl'
             })
             .when('/memes/view-all-the-memes/', {
-                templateUrl: 'views/view-all-memes.html',
+                templateUrl: 'views/memes/view-all-memes.html',
                 controller: 'MainCtrl'
             })
             .when('/cr/', {
