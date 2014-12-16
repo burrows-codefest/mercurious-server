fdb49618e52e876534df7039d02d35b93aa8d9b3
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-12-16 15:23:48 +0000

    get new order id

--- public/scripts/services/bunomaticService.js
+++ public/scripts/services/bunomaticService.js
@@ -4,6 +4,6 @@ angular.module('mercuriousApp')
     .service('bunomaticService', function () {
         this.getNewOrderId = function (str) {
             return str.replace(/[aeiou]/ig,'').replace(' ', '').toUpperCase() +
-                String(Math.floor((Math.random() * 1000) + 1));
+                String(Date.now());
         };
     });
