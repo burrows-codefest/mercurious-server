ccd80b098c7d3ec7ebf2af4e90d21555d81927a2
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-12-16 15:31:51 +0000

    Better formatting

--- public/scripts/services/bunomaticService.js
+++ public/scripts/services/bunomaticService.js
@@ -3,7 +3,6 @@
 angular.module('mercuriousApp')
     .service('bunomaticService', function () {
         this.getNewOrderId = function (str) {
-            return str.replace(/[aeiou]/ig,'').replace(' ', '').toUpperCase() +
-                String(Date.now());
+            return str.replace(/[aeiou]/ig,'').replace(' ', '').toUpperCase() + String(Date.now());
         };
     });
